f25c5f13cd776eb4f70d51ce5390a218712c467f
Dicky Darmawan <dikiidarmawan05x2@gmail.com>
2026-02-28 11:50:54 +0700

    P6

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
     public function profile($id, $name)
     {
         return view('user.profile', compact('id', 'name'));
+    }
+
+    public function index()
+    {
+        // Tambah data pengguna baru dengan Eloquent ORM
+        $data = [
+            'nama' => 'Pelanggan Pertama'
+        ];
+        UserModel::where('username', 'customer-1')->update($data); // Menambahkan data pengguna baru ke tabel m_user
+
+        // Mengambil semua data pengguna
+        $user = UserModel::all(); // Mengambil semua data pengguna dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
