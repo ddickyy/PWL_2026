2c7587b4d40171df4bf1a30a0f9bc2573cd2fbb7
Dicky Darmawan <dikiidarmawan05x2@gmail.com>
2026-03-09 14:48:20 +0700

    P2.5

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -25,16 +25,57 @@ class UserController extends Controller
         // );
         
 
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
-        $user->save();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+        // $user->save();
+
+        // $user = UserModel::create([
+        //     'username' => 'manager44',
+        //     'nama' => 'Manager44',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ]);
+
+        // $user -> username = 'manager45';
+        // $user -> isDirty(); // Memeriksa apakah ada perubahan pada atribut model
+        // $user -> isDirty('username'); // Memeriksa apakah atribut 'username' telah diubah
+        // $user -> isDirty('nama'); // Memeriksa apakah atribut 'nama' telah diubah
+        // $user -> isDirty('nama', 'username'); // Memeriksa apakah atribut 'nama' atau 'username' telah diubah
+        
+        // $user -> isClean(); // false
+        // $user -> isClean('username'); // false
+        // $user -> isClean('nama'); // true
+        // $user -> isClean('nama', 'username'); // false
+
+        // $user -> save(); // Menyimpan perubahan pada model ke database
+        
+        // $user -> isDirty(); // false
+        // $user -> isClean(); // true
+        // dd($user->isDirty());
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]);
+
+        $user -> username = 'manager12';
+
+        $user -> save(); // Menyimpan perubahan pada model ke database
+
+        $user -> wasChanged(); // true
+        $user -> wasChanged('username'); // Memeriksa apakah atribut 'username' telah diubah
+        $user -> wasChanged(['username','level_id']); // Memeriksa apakah atribut 'nama' telah diubah
+        $user -> wasChanged('nama'); // Memeriksa apakah atribut 'nama' telah diubah
+        $user -> wasChanged(['nama', 'username']); // true
+        dd($user->wasChanged(['nama', 'username']));// true
 
         // // Tambah data pengguna baru dengan Eloquent ORM
         // $data = [
